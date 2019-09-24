79077dd3c814023069c880efb8785addffd398ca
Leandre <leandre.blanchard@epitech.eu>
2019-09-24 13:34:12 +0900

    Add sfml-scene module

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ $1 == "debug" ]];
+then
+    mkdir -p build
+    cd build
+    cmake -DCMAKE_BUILD_TYPE=DEBUG ..
+    cmake --build .
+elif [[ $1 == "rebuild" ]];
+then
+    rm -rf build
+    $0
+else
+    mkdir -p build
+    cd build
+    cmake -DCMAKE_BUILD_TYPE=RELEASE ..
+    cmake --build .
+fi;
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+//#include <Scene.hpp>
+
+int main()
+{
+  return 0;
+}
