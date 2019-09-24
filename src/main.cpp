5489dacda3d06a6df2a19eafa519ae143839582c
Leandre <leandre.blanchard@epitech.eu>
2019-09-24 14:23:03 +0900

    [MINOR] empty main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
-//#include <Scene.hpp>
+#include <WScene.hpp>
+#include <GameObject.hpp>
+
+class BasicHandler : public sfs::GameObject {
+	public:
+	void start(sfs::Scene &scene) noexcept
+	{
+		scene.subscribe(*this, sf::Event::Closed);
+	}
+	void onEvent(sfs::Scene &scene, const sf::Event &) noexcept
+	{
+		scene.close();
+	}
+};
 
 int main()
 {
-  return 0;
+	sfs::WScene scene("cautious_fiesta", 120);
+
+	scene.addGameObject<BasicHandler>();
+	scene.run();
+	return 0;
 }
