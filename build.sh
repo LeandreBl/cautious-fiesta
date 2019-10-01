c5c0c913ce2432b1927a08104b0cf2e04ee6902c
Leandre <leandre.blanchard@epitech.eu>
2019-10-02 03:01:44 +0900

    [fix] workaround for sfml dependencies until conan links

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ if [[ $1 == "debug" ]];
 then
     mkdir -p build
     cd build
-    conan install .. --build=missing
     cmake -DCMAKE_BUILD_TYPE=DEBUG ..
     cmake --build .
 elif [[ $1 == "rebuild" ]];
@@ -14,7 +13,6 @@ then
 else
     mkdir -p build
     cd build
-    conan install .. --build=missing
     cmake -DCMAKE_BUILD_TYPE=RELEASE ..
     cmake --build .
 fi;
