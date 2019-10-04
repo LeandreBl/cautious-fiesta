f8011106cd5eca7069c9bb201937c5fa2a6864b3
jb <jean-baptiste.melet@epitech.eu>
2019-10-04 20:20:47 +0200

    [add] layers to the menu

--- src/MainMenu.cpp
+++ src/MainMenu.cpp
@@ -7,8 +7,10 @@ namespace cf
   {
     _scroller = &addChild<sfs::GameObject>(scene);
     _scroller->addChild<Layers>(scene, "assets/sprites/Nuages.jpg", sf::Vector2f(1.2, 1.2), sf::Vector2f(-150, 0));
-    _scroller->addChild<Layers>(scene, "assets/sprites/test.png", sf::Vector2f(1, 2.5), sf::Vector2f(-200, 0), sf::Vector2f(0, 190));
-    _scroller->addChild<Layers>(scene, "assets/sprites/Mimeubles.png", sf::Vector2f(1, 2.5), sf::Vector2f(-300, 0), sf::Vector2f(0, 450));
+    _scroller->addChild<Layers>(scene, "assets/sprites/trainBackground.png", sf::Vector2f(1, 1), sf::Vector2f(-300, 0), sf::Vector2f(0, 930));
+    _scroller->addChild<Layers>(scene, "assets/sprites/test.png", sf::Vector2f(1, 2.5), sf::Vector2f(-250, 0), sf::Vector2f(0, 190));
+    _scroller->addChild<Layers>(scene, "assets/sprites/rangéImeubles.png", sf::Vector2f(1, 1), sf::Vector2f(-325, 0), sf::Vector2f(0, 600));
+    _scroller->addChild<Layers>(scene, "assets/sprites/Mimeubles.png", sf::Vector2f(1, 2.5), sf::Vector2f(-350, 0), sf::Vector2f(0, 515));
     //_scroller->addChild<Layers>(scene, "assets/sprites/imeubles.png", sf::Vector2f(1, 2.5), sf::Vector2f(-350, 0), sf::Vector2f(0, 800));
     _scroller->addChild<Layers>(scene, "assets/sprites/traks.png", sf::Vector2f(1, 1), sf::Vector2f(-500, 0), sf::Vector2f(0, 1040));
     _scroller->addChild<Layers>(scene, "assets/sprites/train.png", sf::Vector2f(0.5, 0.5), sf::Vector2f(550, 0), sf::Vector2f(0, 979));
